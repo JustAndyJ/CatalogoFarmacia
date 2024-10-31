9e30a12823b8755dc927db2d6f44376fffe22fb7
andy <julcac137@gmail.com>
2024-10-30 21:43:03 -0500

    edit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,64 @@
 const products = [
-  { description: "Black lace bodysuit with sexy appeal  with sexy appeal   with sexy appeal with sexy appeal with sexy appeal with sexy appeal with sexy appeal with sexy appeal with sexy appeal with sexy appeal with sexy appealwith sexy appeal  with sexy appeal v with sexy appeal", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/6129.png" },
-  { description: "Elegant bodysuit with floral patterns", type: "body-sexy", price: 30, name: "Body Sexy", image: "imagenes/BodySexy/6154.png" },
-  { description: "Sleeveless lace bodysuit with deep neckline", type: "body-sexy", price: 30, name: "Body Sexy", image: "imagenes/BodySexy/61116.png" },
-  { description: "Classic sexy bodysuit with soft mesh details", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/6188.png" },
-  { description: "Strapless bodysuit in bold colors", type: "body-sexy", price: 30, name: "Body Sexy", image: "imagenes/BodySexy/6141.png" },
-  { description: "Full lace bodysuit with floral accentjjjjjjjjjjjjjjjjjjjjjjjjjjjjs", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/6147.png" },
-  { description: "Seductive bodysuit with plunging neckline", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/6591.png" },
-  { description: "Mesh bodysuit with elegant lace sleeves", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/65737.png" },
-  { description: "Bodysuit with intricate lace details", type: "body-sexy", price: 30, name: "Body Sexy", image: "imagenes/BodySexy/60240Z.png" },
-  { description: "Lace bodysuit with a delicate design", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/61124.png" },
-  { description: "Classic bodysuit with subtle elegance", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/65925.png" },
-  { description: "Lace bodysuit with contemporary flair", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/6159.png" },
-  { description: "Feminine bodysuit with soft, comfortable lace", type: "body-sexy", price: 25, name: "Body Sexy", image: "imagenes/BodySexy/6164.png" },
+  { description: "Crema solar de alta protección diseñada para bloquear rayos UVB y UVA, incluso ultralargos.", 
+    type: "proteccion-Solar", price: 25, name: "La Roche Posay Anthelios Uvmune 400 FPS50 50ml", 
+    image: "imagenes/piel/roche.png" },
+
+  { description: "Limpia e hidrata pieles secas y sensibles, aliviando tirantez e irritación.", 
+    type: "cuidado-piel", price: 30, name: "Aceite de Ducha Bioderma Atoderm 1", 
+    image: "imagenes/piel/atoderm.png" },
+
+  { description: "Gel limpiador purifica y reduce exceso de grasa, ideal para pieles mixtas a grasas propensas a imperfecciones.", 
+    type: "limpiadores", price: 30, name: "Vichy Normaderm Purifying 400ml", 
+    image: "imagenes/piel/normaderm.png" },
+
+  { description: "Gel limpiador suave para pieles sensibles. Limpia sin resecar, ayudando a mantener la barrera cutánea y dejando la piel suave.", 
+    type: "limpiadores", price: 25, name: "La Roche-Posay Lipikar Gel Lavant 400ml", 
+    image: "imagenes/piel/rocheblue.png" },
+
+  { description: "Para piel grasa, reduce el brillo y controla exceso de sebo, proporciona un efecto matificante y ayudando a prevenir imperfecciones.", 
+    type: "cuidado-piel", price: 30, name: "Eucerin DermoPure Oil 50ml", 
+    image: "imagenes/piel/dermopure.png" },
+
+  { description: "Protección contra sudor y malos olores, seca rápidamente y no deja manchas en la ropa. Suave y delicado para la piel.", 
+    type: "desodorantes", price: 25, name: "Desodorante en barra Dove invisible dry 50g", 
+    image: "imagenes/piel/dovedry.png" },
+
+  { description: "proporciona protección duradera contra sudor y malos olores, mientras ayuda a unificar el tono de la piel.", 
+    type: "desodorantes", price: 25, name: "Desodorante Dove Tono Orquidea Uniforme 50g", 
+    image: "imagenes/piel/dovetono.png" },
+
+  { description: "Proporciona protección duradera contra sudor y malos olores. Su textura cremosa asegura una aplicación suave y efectiva.", 
+    type: "desodorantes", price: 25, name: "Desodorante Antitranspirante Crema Etiquet 75g", 
+    image: "imagenes/piel/etiquet.png" },
+    
+  { description: "Con aceite de coco, nutre la piel mientras potencia el bronceado, dejándola suave y con un aroma tropical.", 
+    type: "bronceador", price: 30, name: "Aceite Bronceador Hawaiian Tropic FPS 4 240ml", 
+    image: "imagenes/piel/coco.png" },
+
+  { description: "Crema Corporal que va más allá de las Ceramidas para Piel Seca y Extremadamente Seca", 
+    type: "cuidado-piel", price: 25, name: "Eucerin PH5 Advance Repair Crema Corporal", 
+    image: "imagenes/piel/eucerin.png" },
+
+  { description: "Agua micelar de Bioderma ideal para limpiar y desmaquillar pieles sensibles, dejando la piel fresca y sin irritación.", 
+    type: "cuidado-piel", price: 25, name: "Bioderma Créaline TS H2O 500ml", 
+    image: "imagenes/piel/bioderma.png" },
+
+  { description: "Nutre e hidrata profundamente el cabello, dejándolo suave, brillante y manejable. Ideal para cabello seco o dañado.", 
+    type: "cuidado-cabello", price: 25, name: "Aceite Argan 4 Oils 120ml", 
+    image: "imagenes/piel/arganoil.png" },
+
+  { description: "Limpia a fondo y ayuda a reducir imperfecciones, regulando el exceso de grasa sin resecar la piel.", 
+    type: "limpiadores", price: 25, name: "Cerave Blemish Control Cleanser 236 ml", 
+    image: "imagenes/piel/CeraVe.png" },
+  
+  { description: "Gel limpiador espumoso que elimina suavemente el exceso de grasa y suciedad, ideal para pieles normales a grasas.", 
+    type: "limpiadores", price: 25, name: "CeraVe Foaming Cleanser <br>473ml", 
+    image: "imagenes/piel/seravefoaming.png" },
+
+  { description: "Gel limpiador espumoso que elimina suavemente el exceso de grasa y suciedad, ideal para pieles normales a grasas.", 
+    type: "limpiadores", price: 25, name: "CeraVe Foaming Cleanser <br>236ml", 
+    image: "imagenes/piel/236ml.png" },
+
   ];
 
 function renderProducts() {
@@ -23,10 +70,14 @@ function renderProducts() {
     productElement.className = "product";
     productElement.innerHTML = `
       <img src="${product.image}" alt="${product.name}">
+      <div class="content">
+      <div class="contentName">
       <h2>${product.name}</h2>
+      </div>
       <p class="price">S/.${product.price}</p>
-      <p class="code">Código: ${product.description}</p>
-      <button onclick="copyCode('${product.name}  Código: ${product.id}  Precio: S/.${product.price}')">Copiar Código</button>
+      <p class="code"> ${product.description}</p>
+      <button onclick="copyCode('${product.name}  Precio: S/.${product.price}')">Copiar Nombre</button>
+      </div>
     `;
     catalog.appendChild(productElement);
   });
@@ -34,7 +85,7 @@ function renderProducts() {
 
 function copyCode(code) {
   navigator.clipboard.writeText(code).then(() => {
-    alert("Código copiado: " + code);
+    alert("Producto Copiado: " + code);
   });
 }
 
